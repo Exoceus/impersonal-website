5e18e4e72eec7188ed67b51869566667e95f8fdc
Jatin Mehta <mehta.r.jatin@gmail.com>
2023-02-24 23:58:31 -0500

    small change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 
-import Navbar from "../components/navbar"
+import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
 import About from "../components/About"
 import Projects from "../components/Projects"
