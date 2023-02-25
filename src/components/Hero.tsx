ed20debd6594ec15ae7e12afb36d71d7f07e0cdb
Jatin Mehta <mehta.r.jatin@gmail.com>
2023-02-25 13:49:28 -0500

    hero tagline

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Hero() {
     return (
         <section>
-            Hey, I'm Jatin Mehta
+            <h1>Hey, I'm Jatin Mehta</h1>
+            <h3>Welcome to my small corner of the internet.</h3>
         </section>
     )
 }
