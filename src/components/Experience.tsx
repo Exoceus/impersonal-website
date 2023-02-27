d395d764efc0e82e8de0e6bac99e1923f48f3542
Jatin Mehta <mehta.r.jatin@gmail.com>
2023-02-26 20:17:48 -0500

    load in three js and intial glb

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -16,14 +16,9 @@ export default function Experience() {
             <p>MIT Driverless is a student club collobration between MIT, Pitsburgh, Rocheter and Waterloo that competes in the Indy Autonomous Challenge (Las Vegas, Indiannapolis, Monza).</p>
             <p>Im a part of Simulation and Infrastructure team that aims to optimize the developer experience of the other subteams (Perception, Controls, etc.) by maintaining and updating the tech stack (Docker, ROS2, Asseto Corsa, LGSVL).</p>
 
-            <h1>Fundamentals First</h1>
-            <p>Honestly, I started this blog to force myself to learn technical concepts on a semiregular basis. Since I’m going to read about them on my own time, why not write an article about it? I explore topics I’m curious about while trying to consolidate my learnings into a (mostly) concise and enjoyable introduction to these ideas.</p>
-            <p>Also, I am very prone to slacking of on consistently writing for the blog, so if you dont see any articles published in the past few weeks, remind me please.</p>
-
-            <h1>OppHub</h1>
-            <p>When I moved to Canada I became vaguely aware of the vast number of extrcurricular activities available for students. However, I found it hard to find extracurriculars related to areas I was interested in.</p>
-            <p>Hence, I found myself with a lot of spare time when COVID first hit and I tried to get out of my slump by creating what eveentually became OppHub.The website received 10,000 unique visitors in over 25 different countries with a peak of 600 unique monthly users along with more than 500 unique opportunites.</p>
-            <p>OppHub will always hold a very special place in my heart as it was the first webapp I ever made and first exposed to the sheer possibilities of software development, despite innumerable times I wanted to punch my wall in the process of its development.</p>
+            <h1>Zappos</h1>
+            <p>I worked on experimental AR features to create an interactive online shopping experience during the pandemic for the e-commerce company Zappos (owned by Amazon). I also was a part of the Machine Intelligence team worked on implementing image segmentation models for foot recognition. It was mind boggling watching my work be deployed into the Zappos mobile app beta used by millions of people.</p>
+            <p>The internship strucutre was very unique and unstructured and allowed for rapid experimentation!</p>
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import * as React from "react"
+import React, { useRef, useState } from 'react'
 import type { HeadFC, PageProps } from "gatsby"
+import { Gltf, OrbitControls } from '@react-three/drei'
 
 import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
@@ -9,10 +10,47 @@ import Experience from "../components/Experience"
 import Contact from "../components/Contact"
 import Footer from "../components/Footer"
 
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import * as THREE from 'three'
+
+function Box(props: ThreeElements['mesh']) {
+  const mesh = useRef<THREE.Mesh>(null!)
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  useFrame((state, delta) => (mesh.current.rotation.x += delta))
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+
+
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <div className="page-wrapper">
       <Navbar />
+      <h1>WORK IN PROGRESS! Proceex with caution</h1>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <OrbitControls makeDefault />
+        <Gltf src="models/Shoe.glb" receiveShadow castShadow />
+      </Canvas>
       <div className="inner-wrapper">
         <main>
           <Hero />
